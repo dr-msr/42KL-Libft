2a28783e1c6d7c060c388b64a407458ca2f9cd7e
drmsr <me@drmsr.dev>
2023-10-26 09:43:26 +0800

    init repo

---
+++ ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrahim@42KL | drmsr <me@drmsr.dev>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 20:25:48 by mrahim@42KL       #+#    #+#             */
+/*   Updated: 2023/10/16 17:48:30 by mrahim@42KL      ###   ########.KL       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	output;
+
+	i = 0;
+	sign = 1;
+	output = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+	{
+		i++;
+	}
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		output = (output * 10);
+		output = output + (str[i] - '0');
+		i++;
+	}
+	return (output * sign);
+}
